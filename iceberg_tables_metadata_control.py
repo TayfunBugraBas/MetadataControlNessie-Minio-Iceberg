6d86e6ac42adb78c58419330042cee62c5c0424d
TayfunBugraBas <t.bugra.b@gmail.com>
2025-04-25 15:17:42 +0300

    Update iceberg_tables_metadata_control.py

--- iceberg_tables_metadata_control.py
+++ iceberg_tables_metadata_control.py
@@ -18,41 +18,82 @@ data = response.json()
 bucket_name = "warehouse"
 warehouseArr = []
 
+from minio import Minio
+
+client = Minio(
+    "localhost:9000",
+    access_key="admin",
+    secret_key="password",
+    secure=False
+)
+
+bucket_name = "warehouse"
+warehouseArr = []
+
 try:
     objects = client.list_objects(bucket_name, recursive=True)
 
-    latest_metadata_file = None
-    latest_last_modified = None
-    
+    latest_metadata_per_table = {}
+
     for obj in objects:
+        if not obj.object_name.endswith(".metadata.json"):
+            continue
 
-            metadata = client.stat_object(bucket_name, obj.object_name)
-            last_modified = metadata.last_modified
-            print(str(last_modified) + " " + obj.object_name)
+        metadata = client.stat_object(bucket_name, obj.object_name)
+        last_modified = metadata.last_modified
 
-            
-            if latest_last_modified is None or last_modified > latest_last_modified and obj.object_name.endswith(".metadata.json") :
-                latest_last_modified = last_modified
-                latest_metadata_file = obj.object_name
+       
+        base_name = obj.object_name.replace(".metadata.json", "")
+        table_key = base_name.split("/")[1]  
 
-    warehouseArr.append(latest_metadata_file)
+        
+        if table_key not in latest_metadata_per_table or last_modified > latest_metadata_per_table[table_key]["last_modified"]:
+            latest_metadata_per_table[table_key] = {
+                "object_name": obj.object_name,
+                "last_modified": last_modified
+            }
 
-except S3Error as e:
-    print(f"Error: {e}")
+    warehouseArr = [entry["object_name"] for entry in latest_metadata_per_table.values()]
+
+    print("Bulunan en güncel metadata dosyaları:")
+    for file in warehouseArr:
+        print(file)
+
+except Exception as e:
+    print("Hata:", e)
+
+
 
 for metastore in data["entries"]:
     entry = metastore["content"]
-    print(entry["metadataLocation"])
-    location = entry["metadataLocation"]
-    #found = client.stat_object("warehouse", location.replace("s3a://warehouse",""))
-    #if not found:
-    #    print("bulunamadi")
-    #else:
-    #    print("bu dosya mevcut")
    
-    if  location.replace("s3a://warehouse/","") in warehouseArr:
-        print(location + " bulduuu")
-    else:
-        print(location + "---bulamadii---" )
+    location = entry["metadataLocation"]
+  
+   
+    if  location.replace(f"s3a://{bucket_name}/","") in warehouseArr:
+        print(location + " bulundu dosyalar silinmedi")
+        warehouseArr.remove(location.replace("s3a://warehouse/",""))
 
+#for item in warehouseArr:
+#    i = item.split('/')
+#    base_dir.append(i[0]+ '/' + i[1])
 
+for item in warehouseArr:
+    i = item.split('/')
+    x = client.list_objects(bucket_name, i[0]+ '/' + i[1], recursive=True)
+    for item2 in x:
+        #delete here
+        print("deleting  "+ item2.object_name)
+        client.remove_object(bucket_name, item2.object_name)
+        pass
+
+print(warehouseArr)
+#print(base_dir)
+
+#for_delete = []
+#for item in base_dir:
+#    x = client.list_objects(bucket_name, item, recursive=True)
+#    for item2 in x:
+#        for_delete.append(item2.object_name)
+
+#print(for_delete)
